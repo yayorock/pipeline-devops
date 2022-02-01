13564d800f77200c3e150e8a299325d3d16bc643
Gonzalo Muñoz Boisier <gmboisier@gmail.com>
2022-01-31 21:24:01 -0300

    fix nombres de archivos en contexto

--- bin/main/ejecucion.groovy
+++ bin/main/ejecucion.groovy
@@ -26,12 +26,14 @@ def call(){
                         switch(params.compileTool)
                         {
                             case 'Maven':
-                                def ejecucion = load 'maven.groovy'
-                                ejecucion.call()
+                                //def ejecucion = load 'maven.groovy'
+                                //ejecucion.call()
+                                maven.call()
                             break;
                             case 'Gradle':
-                                def ejecucion = load 'gradle.groovy'
-                                ejecucion.call()
+                                //def ejecucion = load 'gradle.groovy'
+                                //ejecucion.call()
+                                gradle.call()
                             break;
                         }
                     }
