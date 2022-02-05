618c6e9b9ede159108cf81d81e16db7727502420
Gonzalo Muñoz Boisier <gmboisier@gmail.com>
2022-02-04 23:58:06 -0300

    Agrega codigo a los ficheros ci cd

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -51,22 +51,21 @@ def call(){
         nexusPublisher nexusInstanceId: 'nexus',
         nexusRepositoryId: 'devops-usach-nexus',
         packages: [
-            [$class: 'MavenPackage',
+            [
+                $class: 'MavenPackage',
                 mavenAssetList: [
                     [classifier: '',
                     extension: 'jar',
-                    filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar'
-                ]
-            ],
+                    filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']
+                ],
                 mavenCoordinate: [
                     artifactId: 'DevOpsUsach2020',
                     groupId: 'com.devopsusach2020',
                     packaging: 'jar',
-                    version: '0.0.1'
-                ]
+                    version: '0.0.1']
             ]
         ]
-    }
+    }   
 }
 
 return this;
