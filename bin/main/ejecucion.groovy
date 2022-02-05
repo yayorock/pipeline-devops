7f6dbc930429bac224cb87fbb2083c164a4de7c3
Gonzalo Muñoz Boisier <gmboisier@gmail.com>
2022-02-04 23:18:40 -0300

    Agrega codigo a los ficheros ci cd

--- bin/main/ejecucion.groovy
+++ bin/main/ejecucion.groovy
@@ -10,7 +10,7 @@ def call(){
             stage('Pipeline') {
                 steps {
                     script {
-                        sh 'echo "$BRANCH_NAME"'
+                        sh "echo ${env.BRANCH_NAME}"
                         switch("")
                         {
                             case 'develop':
